dab03559a5c8ff3e04ee2f8dee555402ea67ec4e
Racxman90 <155696470+Racxman90@users.noreply.github.com>
2024-02-18 01:05:05 -0300

    Update Pru_3.py

--- Pru_3.py
+++ Pru_3.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from openpyxl import load_workbook
+import pip
+pip.main(["install", "openpyxl"])
 
 # Definir ruta del archivo Excel existente 
 excel_file_path = ''
